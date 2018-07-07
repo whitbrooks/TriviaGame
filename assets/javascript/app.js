d5c285c9e7796c12ecea668e1aa7e599564b4982
whitbrooks <whitbrooks@gmail.com>
2018-07-06 23:04:49 -0700

    first draft of js

---
+++ assets/javascript/app.js
@@ -0,0 +1,78 @@
+// VARIABLES
+var questionOne = {
+    question1: "What is the most visited park in the National Park System?",
+    optionA: "Yosemite National Park",
+    optionB: "Yellowstone National Park",
+    optionC: "Zion National Park",
+    optionD: "Great Smoky Mountains National Park"
+};
+
+var questionTwo = {
+    question2: "What shape is the emblem of the National Park Service?",
+    optionA: "Arrowhead",
+    optionB: "Diamond",
+    optionC: "Log Cabin",
+    optionD: "Redwood tree"
+};
+
+var questionThree = {
+    question3: "Which of the following exists within the boundaries of a national park?",
+    optionA: "The highest point in North America",
+    optionB: "The longest cave system in the world",
+    optionC: "The deepest lake in the US",
+    optionD: "All of the above"
+}
+
+var questionFour = {
+    question4: "Yellowstone National Park's geothermal Morning Glory Pool has changed color over the years. What is responsible for the change?",
+    optionA: "Bird droppings",
+    optionB: "Sulfur evaporation",
+    optionC: "Trash",
+    optionD: "Climate change"
+}
+
+var questionFive = {
+    question5: "Which of the following presidents more than doubled the acreage of the National Park System?",
+    optionA: "Calvin Coolidge",
+    optionB: "Richard Nixon",
+    optionC: "Jimmy Carter",
+    optionD: "George H.W. Bush"
+}
+
+// FUNCTIONS
+
+function nextQuestion() {
+
+    // if current question is questionOne, set form to questionTwo, etc.
+
+    if 
+
+}
+
+function tenSeconds() {
+    
+    // if question is answered correctly, alert "correct!"
+    
+    // else alert "time's up!"
+    alert("time's up!")
+
+    // set form to next question
+    nextQuestion();
+}
+
+
+
+
+
+// CALL FUNCTIONS
+// load html before running script
+$(document).ready(function() {
+
+    setTimeout(tenSeconds, 10000);
+
+
+
+
+
+
+}
